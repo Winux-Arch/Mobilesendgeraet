bc872dff535c2d59dd8f2a93f994e7cb71384f9e
AlienEinstein <61178418+Winux-Arch@users.noreply.github.com>
2023-01-09 15:29:32 +0000

    löschung ungebrauchter dateien und diverse anpassungen

--- Mobi/obj.py
+++
@@ -1,9 +0,0 @@
-class Bildschirmschoner(object):
-    def __init__(self, hintergrund, blase):
-        self.hintergrund = 0x007cb0
-        self.blase = 0x3B83BD
-        return self
-        
-    def activate():
-        # Einar das musst du machen
-        return
