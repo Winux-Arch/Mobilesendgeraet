2b3fbf535af68acfb723dc30a1502e093ce7757a
Heromaex <71891972+Heromaex@users.noreply.github.com>
2023-01-09 09:54:31 +0000

    Dateien hinzugefügt; Bildschirmschoner backend

---
+++ Mobi/obj.py
@@ -0,0 +1,9 @@
+class Bildschirmschoner(object):
+    def __init__(self, hintergrund, blase):
+        self.hintergrund = 0x007cb0
+        self.blase = 0x3B83BD
+        return self
+        
+    def activate():
+        # Einar das musst du machen
+        return
